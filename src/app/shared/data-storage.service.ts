225be4581ebbc3ec7886c8ae2fd2940a09a1d8d0
bhagath <bhagath@getcustomexposure.com>
2018-01-26 12:56:43 +0530

    interceptors

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -14,21 +14,18 @@ export class DataStorageService {
   }
 
   storeRecipes() {
-    const token = this.authService.getToken();
-
+   
    // return this.httpClient.put('https://ng-recipe-book-d98f4.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes(), {
     //  observe: 'events'
    // });
    const req = new HttpRequest('PUT', 'https://ng-recipe-book-d98f4.firebaseio.com/recipes.json',
-    this.recipeService.getRecipes(), {reportProgress: true, params: new HttpParams().set('auth', 
-    token)})
+    this.recipeService.getRecipes(), {reportProgress: true});
      return  this.httpClient.request(req);
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
 
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-d98f4.firebaseio.com/recipes.json?auth=' + token, 
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-d98f4.firebaseio.com/recipes.json', 
      {
         observe: 'body',
         responseType: 'json'
